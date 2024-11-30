a4c939c26e0546b4bfa66e1c59353774038ffdc1
MadhanSaiKrishna <chamarthymadhan.k@students.iiit.ac.in>
2024-11-30 18:12:50 +0530

    Audio 1 Task 1

--- Bird Recognition/test.m
+++ Bird Recognition/test.m
@@ -1,14 +1,1 @@
-% Sample signals
-x = [1, 2, 3, 4, 5];
-y = [2, 3, 4];
-
-% Compute cross-correlation
-[r, lags] = xcorr(x, y);
-
-% Find maximum correlation and its corresponding lag
-[max_corr, index] = max(r);
-lag_at_max_corr = lags(index);
-
-% Display results
-disp(['Maximum Correlation: ', num2str(max_corr)]);
-disp(['Lag at Maximum Correlation: ', num2str(lag_at_max_corr)]);
+%File to test new functions in matlab
